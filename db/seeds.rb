82b2f6c09fce1eb9c999d847965c28a9d1a75fee
yuna <itteq-1109@docomo.ne.jp>
2023-10-14 20:42:39 +0900

    first commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: "a@c",
+User.create!(email: "a@d",
              password:  "080808",
              password_confirmation: "080808",
              admin: true)
